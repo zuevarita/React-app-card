6d439ab9643eb0624ecc3af6fa4a455ae8bc8836
Rita <zuevaritka@gmail.com>
2026-01-28 17:03:07 +0300

    loader component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,8 @@
+import cls from "./Loader.module.css"
+export const Loader = () => {
+    return (  
+        <div className={cls.backdrop}>
+            <span className={cls.loader}></span>
+        </div>
+    );
+}
