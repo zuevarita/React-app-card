6de8a8254b3056d6453bb2ae48b6ac1689e3dcac
Rita <zuevaritka@gmail.com>
2026-02-05 23:49:46 +0300

    added .env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8801";
+export const API_URL = import.meta.env.VITE_SERVER_URL;
 export const AUTH_STORAGE = "reactCardLogin";
 export const THEME_STORAGE = "reactCardTheme";
